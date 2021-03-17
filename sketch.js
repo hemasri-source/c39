f5896746acf6338a9066bb49f3c5a349e93b8f34
hemasri-source <76712464+hemasri-source@users.noreply.github.com>
2021-03-17 17:42:35 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var gameState = 0
+var playerCount
+var form
+var player
+var game
+var allPlayers
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database()
+    game = new Game()
+    game.getState()
+    game.start()
+    
+}
+
+function draw(){
+    background("white");
+    if(playerCount===4){
+        game.update(1)
+    }
+    if (gameState === 1){
+        clear()
+        game.play()
+    }
+
+    
+}
+
